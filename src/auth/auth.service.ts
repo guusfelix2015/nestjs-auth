3edf015ab5fe6941f2648998e218925d5a6541a1
Gustavo Felix <gusta@Air-de-Gustavo.Dlink>
2023-11-29 20:37:59 -0300

    set validations

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
 
-  async signnup() {
+  async signnup(dto: AuthDto) {
+    const { email, password } = dto;
+
+    const foundUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (foundUser) {
+      throw new BadRequestException('Email already exists');
+    }
+
     return { message: 'signup was succefull' };
   }
 
